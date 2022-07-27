5eaa492a985e52b02cd05b80ef295a6474bcc8c2
Robert Norwood <you@example.com>
2022-07-27 00:55:50 -0700

    Created the UI. Done for now, might revisit layout at later time.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ WHITESMOKE = 'whitesmoke'
 
 window = tkinter.Tk()
 window.title('MyPass')
-window.config(padx=20, pady=20, bg=WHITESMOKE)
+window.config(padx=50, pady=50, bg=WHITESMOKE)
 
 
 canvas = tkinter.Canvas(width=200, height=200, bg=WHITESMOKE, highlightthickness=0)
@@ -19,7 +19,35 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
 
+website_label = tkinter.Label(text='Website: ')
+website_label.grid(column=0, row=1)
 
 
+email_label = tkinter.Label(text='Email/Username: ')
+email_label.grid(column=0, row=2)
+
+
+password_label = tkinter.Label(text='Password: ')
+password_label.grid(column=0, row=3)
+
+
+website_input = tkinter.Entry(width=35)
+website_input.grid(column=1, row=1, columnspan=2)
+
+
+email_input = tkinter.Entry(width=35)
+email_input.grid(column=1, row=2, columnspan=2)
+
+
+password_input = tkinter.Entry(width=35)
+password_input.grid(column=1, row=3, columnspan=2)
+
+
+generate_password_button = tkinter.Button(text='Generate Password')
+generate_password_button.grid(column=2, row=3)
+
+
+add_button = tkinter.Button(text='Save Password', bg='light green')
+add_button.grid(column=1, row=4)
 
 window.mainloop()
